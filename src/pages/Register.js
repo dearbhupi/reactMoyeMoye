d3eaee847ff9ade4b60880b1a014660af431834b
Bhupinder Singh <dearbhupi@yahoo.com>
2026-06-11 18:40:06 -0500

    ERROR before userEffect

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,24 +2,21 @@ import React, { useState } from 'react';
 import './Register.css';
 
 function Register() {
-  const [name, setName] = useState('');
+ 
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [age, setAge] = useState(0);
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log("Form submitted successfully!", { name, email });
+    console.log("Form submitted successfully!", { email, password, age });
   }
+
+  setAge(25); // This will cause an error because setAge is not defined 
+
   return (
     <form >
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input className="input-field"
-          id="name"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
+      <h1>age</h1>
       <div>
         <label htmlFor="email">Email:</label>
         <input className="input-field"
@@ -27,6 +24,15 @@ function Register() {
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input className="input-field"
+          id="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
       </div>
       <button
